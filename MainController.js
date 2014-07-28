b1b4e9de88a74a05b60f147c920d8ccf52d6d4fa
Henno van Rensburg <henno_vanrensburg@monex.co.jp>
2014-07-28 13:02:57 +0900

    Added repo logic

--- MainController.js
+++ MainController.js
@@ -1,21 +1,12 @@
 (function() {
-
   var MainController = function($scope, $log, $location) {
-
     $scope.search = function(username) {
       $log.info("MainController says searching for " + username);
       $location.path("/user/" + username);
-
     }
-
     $log.info("MainController initialized...");
   }
 
-
-
-
-
   var app = angular.module("githubViewer");
   app.controller("MainController", ["$scope", "$log", "$location", MainController]);
-
 }()); 

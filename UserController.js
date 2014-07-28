1f419f45ba811307932a6679b9c92de43e08661c
Henno van Rensburg <henno_vanrensburg@monex.co.jp>
2014-07-28 10:20:28 +0900

    Tidy up display and fixed layout

--- UserController.js
+++ UserController.js
@@ -7,10 +7,14 @@
       $scope.user = data;
     }
 
- 
+    var onError = function(error) {
+      $scope.error = error.data.message;
+      $log.info("Api returned an error - " + error.data.message);
+    }
+
     var username = $routeParams.username;
-      $log.info("UserController says searching for " + username);
-      github.getUser(username).then(onUserComplete);
+    $log.info("UserController says searching for " + username);
+    github.getUser(username).then(onUserComplete, onError);
   }
 
   var app = angular.module("githubViewer");
